c09212560cc05250b6197196108050ce96d339e9
Zeshawn Khan <zeshawnk@gmail.com>
2018-07-31 06:57:08 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,25 @@
+var katzDeliLine = [];
+
+ function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name);
+  return "Welcome, " + name + ". You are number "+ katzDeliLine.length + " in line."
+}
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length === 0){
+    return "There is nobody waiting to be served!"
+  }
+  else {
+  var serve = katzDeliLine.shift(0)
+    return "Currently serving " + serve + "."
+  }
+}
+
+function currentLine(katzDeliLine){
+ if (katzDeliLine.length === 0){
+   return "The line is currently empty."
+ }
+  else {
+    return "The line is currently: 1. " + katzDeliLine[0] +", 2. " + katzDeliLine[1] + ", 3. " + katzDeliLine[2]
+  }
+}
